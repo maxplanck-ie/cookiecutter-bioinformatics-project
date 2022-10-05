39f655ffd0064474cc3f8e9ef4565cf0438eb1af
David Koppstein <koppstein@ie-freiburg.mpg.de>
2022-10-05 11:08:12 +0200

    debug

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,17 +1,1 @@
 # credit to: https://github.com/cookiecutter/cookiecutter/issues/723
-
-import os
-import shutil
-
-
-def remove(filepath):
-    if os.path.isfile(filepath):
-        os.remove(filepath)
-    elif os.path.isdir(filepath):
-        shutil.rmtree(filepath)
-
-#provide_dockerfile = '{{cookiecutter.provide_dockerfile}}' == 'yes'
-
-#if not provide_dockerfile:
-    # remove top-level file inside the generated folder
-    # remove('Dockerfile')
